566668f5c08fbd595812fafa94ef82abf3f87523
streaming oldest <125570844+Oldestpagesweb@users.noreply.github.com>
2023-02-16 21:03:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+alert("Bienvenue sur mon site!");
+
+
+
+
+
+
+
+function saveVisitor() {
+  var email = document.getElementById("email").value;
+  // code pour enregistrer l'adresse email du visiteur
+  // ...
+  alert("Votre adresse email a été enregistrée avec succès!");
+}
+
+
+
+
+
+
+
+  document.querySelector('.subscribe-button').addEventListener('click', function() {
+    alert('Vous êtes abonné à la page !');
+  });
+
+
+
+
+const subscribeButton = document.querySelector('.subscribe-button');
+
+subscribeButton.addEventListener('click', () => {
+  alert('Vous êtes abonné à la page!');
+});
+
+
+
+
+
+  ga('create', 'G-0X5DSFG346', 'auto');
+  ga('send', 'pageview');
+  ga('set', 'dimension1', 'value1');
+  ga('set', 'dimension2', 'value2');
+  ga('set', 'dimension3', 'value3');
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QCX3G9KSPC');
+
+
+
+
 
 
 
